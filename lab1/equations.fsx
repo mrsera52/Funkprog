11422a77136f8165a1e794ccb096137b22227414
Serafim Shelaev <sera_04@bk.ru>
2024-09-05 14:50:27 +0300

    Update equations.fsx

--- lab1/equations.fsx
+++ lab1/equations.fsx
@@ -22,19 +22,23 @@ let rec iterations f x0 =
         iterations f x1
 
 
-let f1 x = (3.0 * x) - 4.0 *log x- 5.0
-let f2 x = cos(2.0 / x) - 2.0* sin(1. / x) + 1.0 / x
+let rec newthon f f' x0 =
+    let g x= x - f x / f' x
+    iterations g x0
 
-let f3 x = sqrt(1.0 - 0.4 * x ** 2.0)-asin(x)
+let f1 x = (3.0 * x) - 4.0 *log x- 5.0
+let f2 x = cos(2.0 / x) -2.0* sin(1. / x) + 1.0 / x
+
+let f3 x =sqrt(1.0 - 0.4 * x ** 2.0)-asin(x)
 
 let f1' x = 3.0 - 4.0 / x
-let f2' x = (-2.0 * sin(2.0 / x) + 2.0 * cos(1.0 / x) - 1.0) / (x ** 2.0)
-let f3' x = - (2.0 * sqrt(25.0 - 10.0 * x ** 2.0) *x) / (25.0 -10.0 * x ** 2.0) - (sqrt(1.0 - x ** 2.0) / 1.0 - x ** 2.0)
+let f2' x = (-2.0 * sin(2.0 / x) + 2.0 * cos(1.0 / x)- 1.0) / (x ** 2.0)
+let f3' x = - (2.0 *sqrt(25.0 - 10.0 * x ** 2.0) *x) / (25.0 -10.0 * x ** 2.0) - (sqrt(1.0 - x ** 2.0) / 1.0 - x ** 2.0)
 
 
-let g1 x = x - f1 x / f1' x
-let g2 x = x - f2 x / f2' x
-let g3 x = x - f3 x / f3' x
+//let g1 x = x - f1 x / f1' x
+//let g2 x = x - f2 x / f2' x
+//let g3 x = x - f3 x / f3' x
 
 let phi1 x = (-4.0 *log(x) - 5.0) / 3.0
 let phi2 x = 1.0 / (sin(1.0+ 1.0/x) / (2.0 +2.0 * cos(2.0/x)))
@@ -43,9 +47,9 @@ let phi3 x = sin(sqrt(1.0 - 0.4 * x ** 2.0))
 
 let main =
 
-    printfn " | %10.5f | %10.5f | %10.5f|" (dichotomy f1 2. 4.) (iterations phi1 4.) (iterations g1 2.)
-    printfn " |%10.5f  | %10.5f | %10.5f|" (dichotomy f2  1. 2.) (iterations phi2 1.) (iterations g2 2.)
-    printfn " |%10.5f  | %10.5f | %10.5f|" (dichotomy f3 0. 1.) (iterations phi3 0.) (iterations g3 0.)
+    printfn " | %10.5f | %10.5f | %10.5f|" (dichotomy f1 2. 4.) (iterations phi1 4.) (newthon f1 f1' 2.)
+    printfn " |%10.5f  | %10.5f | %10.5f|" (dichotomy f2  1. 2.) (iterations phi2 1.) (newthon f2 f2' 2.)
+    printfn " |%10.5f  | %10.5f | %10.5f|" (dichotomy f3 0. 1.) (iterations phi3 0.) (newthon f3 f3' 0.)
 
 
 main
