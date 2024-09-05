b81a6d3e8b72d4e3b2d9c44370cb427503b59cb9
Serafim Shelaev <sera_04@bk.ru>
2024-09-05 12:02:41 +0300

    Update equations.fsx

--- lab1/equations.fsx
+++ lab1/equations.fsx
@@ -23,21 +23,21 @@ let rec iterations f x0 =
 
 
 let f1 x = (3.0 * x) - 4.0 * log x - 5.0
-let f2 x = cos(2.0 / x) - 2.0 * sin(1. / x) + 1.0 / x
+let f2 x = cos(2.0 / x) - 2.0* sin(1. / x) + 1.0 / x
 
-let f3 x = sqrt(1.0 - 0.4 * x ** 2.0) - asin(x)
+let f3 x = sqrt(1.0 - 0.4 * x ** 2.0)-asin(x)
 
 let f1' x = 3.0 - 4.0 / x
 let f2' x = (-2.0 * sin(2.0 / x) + 2.0 * cos(1.0 / x) - 1.0) / (x ** 2.0)
-let f3' x = - (2.0 * sqrt(25.0 - 10.0 * x ** 2.0) * x) / (25.0 - 10.0 * x ** 2.0) - (sqrt(1.0 - x ** 2.0) / 1.0 - x ** 2.0)
+let f3' x = - (2.0 * sqrt(25.0 - 10.0 * x ** 2.0) *x) / (25.0 -10.0 * x ** 2.0) - (sqrt(1.0 - x ** 2.0) / 1.0 - x ** 2.0)
 
 
 let g1 x = x - f1 x / f1' x
 let g2 x = x - f2 x / f2' x
 let g3 x = x - f3 x / f3' x
 
-let phi1 x = (-4.0 * log(x) - 5.0) / 3.0
-let phi2 x = 1.0 / (sin(1.0 + 1.0/x) / (2.0 + 2.0 * cos(2.0/x)))
+let phi1 x = (-4.0 *log(x) - 5.0) / 3.0
+let phi2 x = 1.0 / (sin(1.0+ 1.0/x) / (2.0 +2.0 * cos(2.0/x)))
 let phi3 x = sin(sqrt(1.0 - 0.4 * x ** 2.0))
 
 
